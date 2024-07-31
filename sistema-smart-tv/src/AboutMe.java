54e2f29f5054589a2932f50611c6ce507d6c6004
MarceloHaSilva <marcelohenriqueas17@gmail.com>
2024-07-31 18:08:49 -0300

    commit-new

--- sistema-smart-tv/src/AboutMe.java
+++ sistema-smart-tv/src/AboutMe.java
@@ -1,28 +1,33 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
   public static void main(String[] args) {
     // Creating scanner object
-    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-
-    System.out.println("Enter your firstName:");
-    String firstName = scanner.next();
-
-    System.out.println("Enter your lastName:");
-    String lastName = scanner.next();
-
-    System.out.println("Enter your age:");
-    int age = scanner.nextInt();
-
-    System.out.println("Enter your height:");
-    double height = scanner.nextDouble();
-
-
-    //printing the data obtained by the user
-        System.out.println("Hello, my name is " + firstName + " " + lastName);
-        System.out.println("I am " + age + " years ");
-        System.out.println("My height is " + height + "cm ");
-        
+    try{
+      Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+  
+      System.out.println("Enter your firstName:");
+      String firstName = scanner.next();
+  
+      System.out.println("Enter your lastName:");
+      String lastName = scanner.next();
+  
+      System.out.println("Enter your age:");
+      int age = scanner.nextInt();
+  
+      System.out.println("Enter your height:");
+      double height = scanner.nextDouble();
+  
+  
+      //printing the data obtained by the user
+          System.out.println("Hello, my name is " + firstName + " " + lastName);
+          System.out.println("I am " + age + " years ");
+          System.out.println("My height is " + height + "cm ");  
+          scanner.close();
+      }catch(InputMismatchException e) {
+        System.out.println("The age and height fields must be numeric");
+      }
     }
 }
