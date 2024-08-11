ae34b18ada6196669723116cde69d683bf5ec9e6
MarceloHaSilva <marcelohenriqueas17@gmail.com>
2024-08-10 21:02:01 -0300

    commit-new

--- collections-java-api/src/Set/Ordination/Test/ProductRegistration.java
+++ collections-java-api/src/Set/Ordination/Test/ProductRegistration.java
@@ -29,4 +29,25 @@ public class ProductRegistration {
         return productsByPrice;
     }
 
+    public static void main(String[] args) {
+        // Creating an instance of ProductCatalog
+        ProductRegistration productCatalog = new ProductRegistration();
+
+        // Adding products to the catalog
+        productCatalog.addProduct(1L, "Smartphone", 1000d, 10);
+        productCatalog.addProduct(2L, "Notebook", 1500d, 5);
+        productCatalog.addProduct(1L, "Mouse", 30d, 20);
+        productCatalog.addProduct(4L, "Keyboard", 50d, 15);
+
+        // Displaying all products in the catalog
+        System.out.println(productCatalog.productSet);
+
+        // Displaying products sorted by name
+        System.out.println(productCatalog.showProductsByName());
+
+        // Displaying products sorted by price
+        System.out.println(productCatalog.showProductsByPrice());
+    }
+
+
 }
